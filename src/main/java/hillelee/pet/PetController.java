6b422ac85dada7c66342c5ac89f46baa7dd334a7
chp02 <chp02@ukr.net>
2017-11-25 13:05:09 +0200

    Lesson 5 - 25.11.2017 - HTTP methods & statuses

--- src/main/java/hillelee/pet/PetController.java
+++ src/main/java/hillelee/pet/PetController.java
@@ -1,21 +1,112 @@
 package hillelee.pet;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.*;
+import java.util.function.Predicate;
+import java.util.regex.Matcher;
+import java.util.stream.Collectors;
 
 /**
  * Created by JavaEE on 11/18/2017.
  */
 @RestController
-@AllArgsConstructor
 public class PetController {
 
-    private final GreetingProvider greetingProvider;
+//    private List<Pet> pets = new ArrayList<Pet>() {{
+//        add(new Pet("Tom", "Cat", 3));
+//        add(new Pet("Jerry", "Mouse", 1));
+//    }};
+
+    private Integer counter = 1;
+
+    private Map<Integer, Pet> pets = new HashMap<Integer, Pet>() {{
+        put(0, new Pet("Tom", "Cat", 3));
+        put(1, new Pet("Jerry", "Mouse", 1));
+    }};
+
+    //private final GreetingProvider greetingProvider;
     
-    @GetMapping(value = "/greeting")
+    @GetMapping("/greeting")
     public String helloWorld() {
-        return greetingProvider.getRandomGreeting();
+        //return greetingProvider.getRandomGreeting();
+        return "Hello!";
     }
 
+    @GetMapping("/pets")
+    public List<Pet> getPets(@RequestParam Optional<String> specie,
+                             @RequestParam Optional<Integer> age) {
+        Predicate<Pet> specieFilter = specie.map(this::filterBySpecie)
+                .orElse(pet -> true);
+        Predicate<Pet> ageFilter = age.map(this::filterByAge)
+                .orElse(pet -> true);
+        Predicate<Pet> complexFilter = ageFilter.and(specieFilter);
+
+        return pets.values().stream()
+                .filter((complexFilter))
+                .collect(Collectors.toList());
+    }
+
+    private Predicate<Pet> filterBySpecie(String specie) {
+        return pet -> pet.getSpecie().equals(specie);
+    }
+
+    private Predicate<Pet> filterByAge(Integer age) {
+        return pet -> pet.getAge().equals(age);
+    }
+
+    @GetMapping("/pets/{id}")
+    public ResponseEntity<?> getPetById(@PathVariable Integer id) {
+        if (id >= pets.size()) {
+            return ResponseEntity.badRequest()
+                    .body(new ErrorBody("no pet with ID = " + id));
+        }
+        return ResponseEntity.ok(pets.get(id));
+    }
+
+    @PostMapping("/pets")
+    public ResponseEntity<Void> createPet(@RequestBody Pet pet) {
+        pets.put(++counter, pet);
+        return ResponseEntity.created(URI.create("/pets/" + counter)).build();
+    }
+
+    @PutMapping("/pets/{id}")
+    public void updatePet(@PathVariable Integer id, @RequestBody Pet pet) {
+        pets.put(id, pet);
+    }
+
+    @DeleteMapping("/pets/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletePet(@PathVariable Integer id) {
+        if (!pets.containsKey(id)) {
+            throw new NoSuchPetException();
+        }
+        pets.remove(id.intValue());
+    }
+
+}
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+class NoSuchPetException extends RuntimeException {}
+
+@Data
+@AllArgsConstructor
+class ErrorBody {
+    private String message;
+    private final Integer code = 400;
+}
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class Pet {
+    private String name;
+    private String specie;
+    private Integer age;
 }
