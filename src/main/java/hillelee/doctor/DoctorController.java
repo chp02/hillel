af3880de0dbebd2d3874679169f7e8a85416101f
chp02@ukr.net <chp02@ukr.net>
2017-11-27 19:33:56 +0200

    returns Doctor

--- src/main/java/hillelee/doctor/DoctorController.java
+++ src/main/java/hillelee/doctor/DoctorController.java
@@ -1,8 +1,16 @@
 package hillelee.doctor;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by dmitriy.chebotarev@hpe.com on 11/27/2017.
  */
+@RestController
 public class DoctorController
 {
+  @GetMapping("/doctor")
+  public Doctor get() {
+    return new Doctor(1, "Vasia", "zubnyuk");
+  }
 }
